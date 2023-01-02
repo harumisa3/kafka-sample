ece916901e82251b0bbcaf9acba4bdff17c2ad94
mharuta <mharuta@netprotections.co.jp>
2023-01-02 19:09:46 +0900

    fix: add module

--- src/hello/hello.module.ts
+++ src/hello/hello.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { HelloConsumer } from './hello.consumer';
+import { HelloService } from './hello.service';
 
-@Module({})
+@Module({
+  providers: [HelloConsumer, HelloService],
+})
 export class HelloModule {}
